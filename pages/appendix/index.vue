87abe2702f260b9c3cc55bef126c8fefb68d9bea
Nicholas Stefanski <nmstefanski@gmail.com>
2023-08-17 10:59:40 -0500

    Create appendix/index.vue

---
+++ pages/appendix/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="docs-container container">
+    <h1>Appendix</h1>
+    <div class="docs-toc">
+      <ul v-if="appendixSections">
+        <li v-for="section in appendixSections" :key="section.slug">
+          <nuxt-link tag="a" :to="`/appendix/${section.slug}`">
+            {{ section.name }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+import { useMainStore } from '~/store';
+
+export default {
+  setup() {
+    const store = useMainStore();
+    return { store };
+  },
+  computed: {
+    appendixSections: function () {
+      return this.store.allAppendixSections;
+    },
+  },
+};
+</script>
+
+<style></style>
