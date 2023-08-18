6bfb2f78eeb8d2d12fcacd6a2b6573b797f25cef
Nicholas Stefanski <nmstefanski@gmail.com>
2023-08-18 13:07:39 -0500

    Create planes/[id].vue

---
+++ pages/planes/[id].vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="docs-container container">
+    <h1>{{ plane.name }}</h1>
+    <md-viewer :text="plane.desc" />
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+import MdViewer from '~/components/MdViewer';
+
+export default {
+  components: {
+    MdViewer,
+  },
+  data() {
+    return {
+      posts: [],
+      errors: [],
+      plane: [],
+    };
+  },
+  mounted() {
+    return axios
+      .get(
+        `${useRuntimeConfig().public.apiUrl}/planes/${
+          this.$route.params.id
+        }`
+      ) //you will need to enable CORS to make this work
+      .then((response) => {
+        this.plane = response.data;
+      });
+  },
+};
+</script>
+
+<style lang="scss"></style>
